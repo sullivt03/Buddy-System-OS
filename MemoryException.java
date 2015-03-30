5487c65954164cb898a03727f9c061f963398fa8
sullivt03 <sulliv61@students.rowan.edu>
2015-03-30 19:14:26 -0400

    Update MemoryException.java

--- MemoryException.java
+++ MemoryException.java
@@ -1,5 +1,8 @@
 package memoryMgmtSystem;
 
+/**
+ * Exception to make sure the function trying to be performed can be executed.
+ **/
 public class MemoryException extends RuntimeException {
 	
 	public MemoryException (String s) {
